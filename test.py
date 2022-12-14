20e37ca44174fd497643f9acfad7f0d9b43cea46
Federico Falcon <ffalcondepizzo@gmail.com>
2022-12-14 14:02:18 -0300

    add ofi 14-12 testeando

---
+++ test.py
@@ -0,0 +1,60 @@
+import json
+import urllib.request
+
+# json_text = """
+# [
+#     { "id": 0, "nombre": "Remera Halloween", "precio": 1500 },
+#     { "id": 1, "nombre": "Remera Navidad", "precio": 2000 }
+# ]
+# """
+
+# diccionario = json.loads(json_text)
+# print(type(diccionario))
+# print(type(json_text))
+# print(diccionario[0])
+# print(type(diccionario[0]["nombre"]))
+
+# for i in diccionario:
+#     print(i["id"], end=" - ")
+#     print(i["nombre"], end=" - $")
+#     print(i["precio"])
+
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+# nuevoString = ""
+
+# urlhandler = urllib.request.urlopen("https://www.mockachino.com/b95175d8-0940-4e/users")
+# for i in urlhandler:
+#     # print(type(i))
+#     # print(i)
+#     # print(type(i.decode()))
+#     # print(i.decode())
+#     nuevoString = nuevoString + i.decode()
+
+# print(type(nuevoString))
+# print(nuevoString)
+
+# arrayJson = json.loads(nuevoString)
+# print(type(arrayJson))
+# print(arrayJson)
+
+# for i in arrayJson["users"]:
+#     print(i["first_name"])
+
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////////////////////////////////////////////
+# Manera elegante
+
+urlhandler = urllib.request.urlopen("https://www.mockachino.com/b95175d8-0940-4e/users")
+for i in urlhandler:
+    arrayJson2 = json.loads(i)
+
+print(type(arrayJson2))
+print(arrayJson2)
+
+for i in arrayJson2["users"]:
+    print(i["first_name"])
